4574e1f857f6dbb699ea6201ada092b0c674569d
liusCoding <14786469221@163.com>
2020-05-02 10:17:40 +0800

    分页插件配置

--- service/service_edu/src/main/java/com/liuscoding/edu/config/EduConfig.java
+++ service/service_edu/src/main/java/com/liuscoding/edu/config/EduConfig.java
@@ -2,6 +2,7 @@ package com.liuscoding.edu.config;
 
 import com.baomidou.mybatisplus.core.injector.ISqlInjector;
 import com.baomidou.mybatisplus.extension.injector.LogicSqlInjector;
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -24,6 +25,14 @@ public class EduConfig {
     @Bean
     public ISqlInjector sqlInjector() {
         return new LogicSqlInjector();
+    }
+
+    /**
+     * 分页插件
+     */
+    @Bean
+    public PaginationInterceptor paginationInterceptor() {
+        return new PaginationInterceptor();
     }
 
 }
