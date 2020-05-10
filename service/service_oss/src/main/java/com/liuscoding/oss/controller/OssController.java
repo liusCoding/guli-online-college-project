85f7e7ddd48b8e50729466134f146167a6e9af61
liusCoding <14786469221@163.com>
2020-05-10 10:23:23 +0800

    修改阿里云OSSController路径

--- service/service_oss/src/main/java/com/liuscoding/oss/controller/OssController.java
+++ service/service_oss/src/main/java/com/liuscoding/oss/controller/OssController.java
@@ -5,6 +5,7 @@ import com.liuscoding.oss.service.IOssService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,7 +20,8 @@ import org.springframework.web.multipart.MultipartFile;
 
 @Api(tags = "阿里云OSS文件上传")
 @RestController
-@RequestMapping("/edu/oss")
+@RequestMapping("/eduoss/file")
+@CrossOrigin
 public class OssController {
 
     private final IOssService ossService;
